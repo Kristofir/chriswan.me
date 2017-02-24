b37c083eac5a496d12fdd94040c29963fcc3e181
Chris Wan <hello@chriswan.me>
2017-02-23 17:02:12 -0800

    updated

--- scripts/box.js
+++ scripts/box.js
@@ -1,9 +1,12 @@
-$('.hide-content').hide();
+// Hide all the elements in the DOM that have a class of "box"
+$('.box').hide();
 
-$('.show-content').each(function() {
-    $(this).show(0).on('click', function(e) {
-        e.preventDefault();
-
-        $(this).next('.hide-content').slideToggle('fast');
+// Make sure all the elements with a class of "clickme" are visible and bound
+// with a click event to toggle the "box" state
+$('.clickme').each(function() {
+  $(this).show(0).on('click', function() {
+    $(this).next('.box').slideToggle('fast', function() {
+        $(this).prev().html($(this).is(':visible') ? 'Hide' : 'Show');
     });
+  });
 });
